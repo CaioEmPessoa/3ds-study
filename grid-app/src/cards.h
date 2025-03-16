08a7a2b60e7f2a2b803c1060901940342a4ab165
Caio Polonio <127911795+CaioEmPessoa@users.noreply.github.com>
2025-03-16 17:14:54 -0300

    added header file with the cards info

---
+++ grid-app/src/cards.h
@@ -0,0 +1,7 @@
+#define int cardsPos[102][3][18] = {
+   [2, 1, [1, 2]] // card width /  special point location / squares with ink, see  the original card for reference
+}
+
+#define String cardsStyle[102][2] = {
+   ["Tentatek", "Tentatek.png"] // name, img location
+}
